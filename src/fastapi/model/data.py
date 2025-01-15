2d2a564b0eca52068dddac1018b30a1a7c10fec0
Hleb Stenin <hlebstenin@gmail.com>
2025-01-15 21:14:57 +0300

    feat: debugged data collection and added model pipeline

--- src/fastapi/model/data.py
+++ src/fastapi/model/data.py
@@ -1,61 +1,102 @@
 import re
+import requests
+from zipfile import ZipFile
+from io import BytesIO
 
+import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from omegaconf import DictConfig
-import kagglehub
+from sklearn.model_selection import train_test_split
+
+from model.paths import CACHE_DIR
+
+
+def load_data(cfg):
+
+    print("started loading data")
+
+    raw_data = get_raw_data(cfg)
+
+    data = preprocess(cfg, raw_data)
+    data_train, data_test = split(cfg, data)
+    data_train, data_test = transform(cfg, data_train, data_test)
+
+    data_train.to_pickle(CACHE_DIR / "data-train.pkl")
+    data_test.to_pickle(CACHE_DIR / "data-test.pkl")
+
+    print("finished loading data")
 
 
 def get_raw_data(cfg: DictConfig) -> pd.DataFrame:
 
-    path = kagglehub.dataset_download(
-        "beridzeg45/watch-prices-dataset",
-        path="watches (cleaned).csv",
-    )
+    path = CACHE_DIR / "watches (cleaned).csv"
+
+    if not path.exists():
+
+        url = "https://www.kaggle.com/api/v1/datasets/download/beridzeg45/watch-prices-dataset"
+        response = requests.get(url, verify=False)
+        response.raise_for_status()
+
+        with ZipFile(BytesIO(response.content)) as file:
+            file.extractall(CACHE_DIR)
 
     raw_data = pd.read_csv(path)
 
     return raw_data
 
 
-def get_data(cfg: DictConfig) -> pd.DataFrame:
+def preprocess(cfg: DictConfig, raw_data: pd.DataFrame) -> pd.DataFrame:
 
-    raw_data = get_raw_data()
     data = raw_data.copy()
+
+    print(f"{len(data)=}")
 
     columns = {column: snake_case(column) for column in data.columns}
     data = data.rename(columns=columns)
 
-    relevant_columns = [
-        "brand",
-        "movement",
-        "condition",
-        "scope_of_delivery",
+    float_columns = [
+        "price",
         "water_resistance",
         "face_area",
+        "watches_sold_by_the_seller",
+        "active_listing_of_the_seller",
+        "seller_reviews",
+        "year_of_production",
+    ]
+    categorical_columns = [
+        "brand",
+        "movement",
         "case_material",
         "bracelet_material",
+        "condition",
+        "scope_of_delivery",
         "gender",
+        "availability",
         "shape",
         "crystal",
         "dial",
         "bracelet_color",
         "clasp",
+    ]
+    boolean_columns = [
         "fast_shipper",
         "trusted_seller",
         "punctuality",
-        "price",
-        # 'year_of_production',
-        # 'availability',
-        # 'watches_sold_by_the_seller',
-        # 'active_listing_of_the_seller',
-        # 'seller_reviews'
     ]
 
-    for column in relevant_columns:
+    input_columns = float_columns + categorical_columns + boolean_columns
+
+    for column in input_columns:
         assert column in data.columns, f"{column} not in data"
 
-    data = data[relevant_columns]
+    data = data[input_columns]
+
+    dtypes = {
+        **{column: float for column in float_columns},
+        **{column: "category" for column in categorical_columns},
+        **{column: bool for column in boolean_columns},
+    }
+    data = data.astype(dtypes)
 
     optional_columns = [
         "movement",
@@ -67,26 +108,63 @@ def get_data(cfg: DictConfig) -> pd.DataFrame:
         "dial",
         "bracelet_color",
         "clasp",
+        "availability",
     ]
-    data = data.fillna({column: "Undefined" for column in optional_columns})
+
+    for column in categorical_columns:
+        data[column] = (
+            data[column]
+            .cat.add_categories(["Undefined", "Infrequent"])
+            .fillna("Undefined")
+        )
 
     data = data.fillna(
         {
             "water_resistance": 0,
             "face_area": 700,
+            "watches_sold_by_the_seller": 0,
+            "active_listing_of_the_seller": 1,
+            "seller_reviews": 0,
         }
     )
+
+    decades = [1980, 1990, 2000, 2010, 2020]
+    for decade in decades:
+        data[f"produced_before_{decade}"] = np.where(
+            data["year_of_production"].isna(),
+            False,
+            data["year_of_production"] < decade,
+        )
+
+    data["produced_in_2024"] = np.where(
+        data["year_of_production"].isna(),
+        False,
+        data["year_of_production"] == 2024,
+    )
+
+    data = data.drop(columns=["year_of_production"])
+
+    data["original_box"] = data["scope_of_delivery"].isin(
+        [
+            "Original box, original papers",
+            "Original box, no original papers",
+        ]
+    )
+    data["original_papers"] = data["scope_of_delivery"].isin(
+        [
+            "Original box, original papers",
+            "Original papers, no original box",
+        ]
+    )
+
+    # data["face_area"] = np.clip(data["face_area"], cfg.min_face_area, cfg.max_face_area)
+    data = data[data["face_area"].between(cfg.min_face_area, cfg.max_face_area)]
+    data = data[data["price"].between(cfg.min_price, cfg.max_price)]
 
     data = data.dropna()
     data = data.drop_duplicates()
 
-    data = data.astype(
-        {
-            "fast_shipper": bool,
-            "trusted_seller": bool,
-            "punctuality": bool,
-        }
-    )
+    print(f"{len(data)=}")
 
     return data
 
@@ -99,12 +177,68 @@ def snake_case(text: str) -> str:
     return text
 
 
-def split_data(cfg: DictConfig, data: pd.DataFrame) -> pd.DataFrame:
+def split(cfg: DictConfig, data: pd.DataFrame) -> tuple[pd.DataFrame]:
 
-    test_size = cfg.test_size
-    val_size = cfg.val_size / (1 - test_size)
+    data_train, data_test = train_test_split(
+        data,
+        test_size=cfg.test_size,
+        shuffle=True,
+        random_state=0,
+    )
 
-    data_train, data_test = train_test_split(data, test_size=test_size)
-    data_train, data_val = train_test_split(data_train, test_size=val_size)
+    return data_train, data_test
 
-    return data_train, data_val, data_test
+
+def transform(
+    cfg: DictConfig,
+    data_train: pd.DataFrame,
+    data_test: pd.DataFrame,
+) -> tuple[pd.DataFrame]:
+
+    # Fit stage
+    brands = data_train.groupby(by="brand", observed=True).agg(
+        price_min=pd.NamedAgg(column="price", aggfunc="min"),
+        price_max=pd.NamedAgg(column="price", aggfunc="max"),
+        price_median=pd.NamedAgg(column="price", aggfunc="median"),
+        count=pd.NamedAgg(column="price", aggfunc="count"),
+    )
+
+    # brands_count = brands["count"]
+    # brands["popularity"] = (brands_count - brands_count.min()) / (
+    #     brands_count.max() - brands_count.min()
+    # )
+
+    brands["tier"] = None
+    brands_price = brands["price_median"]
+    quantiles = np.quantile(
+        brands_price,
+        q=np.linspace(0, 1, cfg.n_brand_tiers + 1),
+    )
+    for i in range(cfg.n_brand_tiers):
+        brands.loc[brands_price.between(quantiles[i], quantiles[i + 1]), "tier"] = i
+
+    brands["range"] = brands["price_max"] / brands["price_min"]
+
+    infrequent_brands = brands[brands["count"] < cfg.min_brand_count].index.tolist()
+
+    # Transform stage
+    data_train["brand_count"] = data_train["brand"].map(brands["count"])
+    data_test["brand_count"] = data_test["brand"].map(brands["count"])
+
+    # data_train["brand_popularity"] = data_train["brand"].map(brands["popularity"])
+    # data_test["brand_popularity"] = data_test["brand"].map(brands["popularity"])
+
+    data_train["brand_tier"] = data_train["brand"].map(brands["tier"])
+    data_test["brand_tier"] = data_test["brand"].map(brands["tier"])
+
+    data_train["brand_range"] = data_train["brand"].map(brands["range"])
+    data_test["brand_range"] = data_test["brand"].map(brands["range"])
+
+    print(f"{len(data_train['brand'].unique())=}")
+
+    data_train.loc[data_train["brand"].isin(infrequent_brands), "brand"] = "Infrequent"
+    data_test.loc[data_test["brand"].isin(infrequent_brands), "brand"] = "Infrequent"
+
+    print(f"{len(data_train['brand'].unique())=}")
+
+    return data_train, data_test
--- src/fastapi/model/model.py
+++ src/fastapi/model/model.py
@@ -1,1 +1,242 @@
+import os
+import json
 
+import joblib
+import numpy as np
+import pandas as pd
+from omegaconf import OmegaConf, DictConfig
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler, StandardScaler
+from sklearn.metrics import (
+    mean_squared_error,
+    mean_absolute_percentage_error,
+    mean_squared_log_error,
+    r2_score,
+)
+from sklearn.compose import (
+    ColumnTransformer,
+    TransformedTargetRegressor,
+    make_column_selector,
+)
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import GradientBoostingRegressor, HistGradientBoostingRegressor
+from sklearn.inspection import permutation_importance
+
+from model.paths import CACHE_DIR
+
+
+SUPPORTED_MODEL_TYPES = {
+    # "linear_regression",
+    # "decision_tree",
+    "gradient_boosting",
+}
+
+MIN_PRICE = 0
+MAX_PRICE = 1e6
+
+MIN_LOG_PRICE = None
+MAX_LOG_PRICE = np.log1p(MAX_PRICE)
+
+
+def load_models(cfg):
+
+    print("started loading models")
+
+    data_train = pd.read_pickle(CACHE_DIR / "data-train.pkl")
+    data_test = pd.read_pickle(CACHE_DIR / "data-test.pkl")
+
+    X_train = data_train[cfg.features]
+    y_train = data_train[cfg.target]
+
+    X_test = data_test[cfg.features]
+    y_test = data_test[cfg.target]
+
+    print(X_train.shape, X_test.shape)
+
+    for model_type in SUPPORTED_MODEL_TYPES:
+        model = get_model(model_type)
+
+        print(X_train.info())
+
+        model.fit(X_train, y_train)
+
+        metrics = {
+            "train": compute_metrics(model, X_train, y_train),
+            "test": compute_metrics(model, X_test, y_test),
+        }
+
+        print(json.dumps(metrics, indent=4))
+
+        importances = {
+            "train": compute_importances(model, X_train, y_train),
+            "test": compute_importances(model, X_test, y_test),
+        }
+
+        # print(json.dumps(importances, indent=4))
+
+        model_dir = CACHE_DIR / model_type
+        model_dir.mkdir(parents=True, exist_ok=True)
+
+        joblib.dump(model, model_dir / "model.pkl")
+
+        with open(model_dir / "metrics.json", "w") as file:
+            json.dump(
+                metrics,
+                file,
+                ensure_ascii=False,
+                indent=4,
+            )
+
+        with open(model_dir / "importances.json", "w") as file:
+            json.dump(
+                importances,
+                file,
+                ensure_ascii=False,
+                indent=4,
+            )
+
+    print("finished loading models")
+
+
+def get_model(model_type) -> Pipeline:
+
+    if not model_type in SUPPORTED_MODEL_TYPES:
+        raise ValueError(f"Specified model type `{model_type}` is not supported.")
+
+    if model_type == "gradient_boosting":
+        model = get_gradient_boosting_model()
+
+    return model
+
+
+def get_gradient_boosting_model():
+
+    # Create and train the model
+    regressor = HistGradientBoostingRegressor(
+        loss="squared_error",
+        learning_rate=0.1,
+        max_iter=100,
+        max_leaf_nodes=None,
+        max_depth=None,
+        min_samples_leaf=30,
+        max_features=1.0,
+        categorical_features="from_dtype",
+        warm_start=True,
+    )
+
+    model = TransformedTargetRegressor(
+        regressor=regressor,
+        func=log_transform,
+        inverse_func=exp_transform,
+    )
+
+    # Create a pipeline with preprocessing and the model
+    pipeline = Pipeline(
+        steps=[
+            ("model", model),
+        ]
+    )
+
+    return pipeline
+
+
+def get_linear_regression_model():
+
+    # Combine transformers using ColumnTransformer
+    transformer = ColumnTransformer(
+        transformers=[
+            # (
+            #     "categorical",
+            #     OneHotEncoder(handle_unknown="ignore", drop="first", min_frequency=50),
+            #     make_column_selector(dtype_include="category"),
+            # ),
+            (
+                "numerical",
+                MinMaxScaler(),
+                make_column_selector(dtype_include=np.float64),
+            ),
+        ],
+        sparse_threshold=0,
+        verbose_feature_names_out=True,
+    )
+
+    # Create and train the model
+    regressor = HistGradientBoostingRegressor(
+        loss="squared_error",
+        learning_rate=0.1,
+        max_iter=100,
+        max_leaf_nodes=None,
+        max_depth=None,
+        min_samples_leaf=30,
+        max_features=1.0,
+        categorical_features="from_dtype",
+        warm_start=True,
+    )
+
+    model = TransformedTargetRegressor(
+        regressor=regressor,
+        func=log_transform,
+        inverse_func=exp_transform,
+    )
+
+    # Create a pipeline with preprocessing and the model
+    pipeline = Pipeline(
+        steps=[
+            ("transformer", None),
+            ("model", model),
+        ]
+    )
+
+    return pipeline
+
+
+def log_transform(array):
+    return np.log1p(np.clip(array, MIN_PRICE, MAX_PRICE))
+
+
+def exp_transform(array):
+    return np.expm1(np.clip(array, MIN_LOG_PRICE, MAX_LOG_PRICE))
+
+
+def compute_metrics(model, X, y):
+
+    y_pred = model.predict(X)
+
+    metrics = {
+        "mean_squared_error": mean_squared_error(y, y_pred),
+        "mean_absolute_percentage_error": mean_absolute_percentage_error(y, y_pred),
+        "r2_score": r2_score(y, y_pred),
+    }
+
+    return metrics
+
+
+def compute_importances(model, X, y):
+
+    outputs = permutation_importance(
+        model,
+        X,
+        y,
+        n_repeats=5,
+        max_samples=0.75,
+        n_jobs=-1,
+        random_state=0,
+    )
+
+    importances_mean = outputs.importances_mean
+    importances_std = outputs.importances_std
+
+    importances = [
+        {
+            "feature": model.feature_names_in_[i],
+            "importance_mean": importances_mean[i],
+            "importance_std": importances_std[i],
+        }
+        for i in range(len(model.feature_names_in_))
+    ]
+
+    importances = sorted(importances, key=lambda item: -item["importance_mean"])
+
+    return importances
